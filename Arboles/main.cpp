15b042da6a4a4ec207ef993a4eb121a963030fab
Turupawn <ahmed.hn.43@gmail.com>
2015-06-03 17:30:00 -0600

    backtracking 2

--- Arboles/main.cpp
+++ Arboles/main.cpp
@@ -23,20 +23,81 @@ public:
     Arbol()
     {
         this->raiz=NULL;
-        Nodo* n1 = new Nodo(1);
-        Nodo* n2 = new Nodo(2);
-        Nodo* n3 = new Nodo(3);
-        Nodo* n4 = new Nodo(4);
-        Nodo* n5 = new Nodo(5);
-        Nodo* n6 = new Nodo(6);
+//        Nodo* n15 = new Nodo(15);
+//        Nodo* n6 = new Nodo(6);
+//        Nodo* n4 = new Nodo(4);
+//        Nodo* n5 = new Nodo(5);
+//        Nodo* n7 = new Nodo(7);
+//        Nodo* n23 = new Nodo(23);
+//        Nodo* n71 = new Nodo(71);
+//        Nodo* n50 = new Nodo(50);
+//
+//        n15->hijo_izq = n6;
+//        n15->hijo_der = n23;
+//        n6->hijo_izq = n4;
+//        n6->hijo_der = n7;
+//        n4->hijo_der = n5;
+//        n23->hijo_der = n71;
+//        n71->hijo_izq = n50;
+//
+//        raiz=n15;
+    }
 
-        n1->hijo_izq = n2;
-        n1->hijo_der = n3;
-        n2->hijo_izq = n4;
-        n2->hijo_der = n5;
-        n4->hijo_izq = n6;
+    void agregar(Nodo* nuevo)
+    {
+        agregar(&raiz,nuevo);
+    }
 
-        raiz=n1;
+    void agregar(Nodo* actual,Nodo* nuevo)
+    {
+        if(nuevo->valor<actual->valor)
+        {
+            if(actual->hijo_izq==NULL)
+            {
+                actual->hijo_izq = nuevo;
+            }else
+            {
+                agregar(actual->hijo_izq,nuevo);
+            }
+        }else
+        {
+            if(actual->hijo_der==NULL)
+            {
+                actual->hijo_der=nuevo;
+            }else
+            {
+                agregar(actual->hijo_der,nuevo);
+            }
+        }
+    }
+
+    void agregar(Nodo** actual,Nodo* nuevo)
+    {
+        if(*actual==NULL)
+        {
+            *actual=nuevo;
+        }else
+        {
+            if(nuevo->valor<(*actual)->valor)
+            {
+                if((*actual)->hijo_izq==NULL)
+                {
+                    (*actual)->hijo_izq = nuevo;
+                }else
+                {
+                    agregar((*actual)->hijo_izq,nuevo);
+                }
+            }else
+            {
+                if((*actual)->hijo_der==NULL)
+                {
+                    (*actual)->hijo_der=nuevo;
+                }else
+                {
+                    agregar((*actual)->hijo_der,nuevo);
+                }
+            }
+        }
     }
 
     void imprimir(Nodo* actual)
@@ -59,8 +120,39 @@ public:
         return actual->valor + izq + der;
     }
 
-    bool existe(Nodo* actual, int valor_buscado)
+    bool buscar(Nodo* actual, int valor_buscado)
     {
+        if(actual==NULL)
+            return false;
+
+        cout<<"Buscando"<<endl;
+
+        if(actual->valor==valor_buscado)
+            return true;
+
+        if(buscar(actual->hijo_izq,valor_buscado))
+            return true;
+
+        if(buscar(actual->hijo_der,valor_buscado))
+            return true;
+
+        return false;
+    }
+
+    bool buscarAVL(Nodo* actual, int valor_buscado)
+    {
+        if(actual==NULL)
+            return false;
+
+        cout<<"Buscando AVL"<<endl;
+
+        if(actual->valor == valor_buscado)
+            return true;
+
+        if(actual->valor < valor_buscado)
+            return buscarAVL(actual->hijo_der,valor_buscado);
+
+        return buscarAVL(actual->hijo_izq,valor_buscado);
 
     }
 };
@@ -68,8 +160,18 @@ public:
 int main()
 {
     Arbol a;
+    a.agregar(new Nodo(15));
+    a.agregar(new Nodo(6));
+    a.agregar(new Nodo(4));
+    a.agregar(new Nodo(5));
+    a.agregar(new Nodo(7));
+    a.agregar(new Nodo(23));
+    a.agregar(new Nodo(71));
+    a.agregar(new Nodo(50));
+
     a.imprimir(a.raiz);
-    cout<<a.sumar(a.raiz);
+    cout<<a.buscar(a.raiz,50)<<endl;
+    cout<<a.buscarAVL(a.raiz,50)<<endl;
 
     return 0;
 }
---
+++ Backtracking2/main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+using namespace std;
+
+bool existeCamino(char laberinto[5][5],
+                    int inicio_x,int inicio_y,
+                    int destino_x,int destino_y)
+{
+    cout<<"Test"<<endl;
+    if(laberinto[inicio_y][inicio_x]=='*'
+       || laberinto[destino_y][destino_x]=='*')
+       return false;
+
+    if(inicio_x<0
+       || inicio_y<0
+       || inicio_x>4
+       || inicio_y>4
+       || destino_y<0
+       || destino_x>4
+       || destino_y>4)
+        return false;
+
+    if(inicio_x == destino_x
+       && inicio_y == destino_y)
+       return true;
+
+    if(existeCamino(laberinto,
+                     inicio_x,inicio_y+1,
+                     destino_x,destino_y))
+        return true;
+
+    if(existeCamino(laberinto,
+                     inicio_x+1,inicio_y,
+                     destino_x,destino_y))
+        return true;
+
+    if(existeCamino(laberinto,
+                     inicio_x,inicio_y-1,
+                     destino_x,destino_y))
+        return true;
+
+    if(existeCamino(laberinto,
+                     inicio_x-1,inicio_y,
+                     destino_x,destino_y))
+        return true;
+
+    return false;
+}
+
+int main()
+{
+    char laberinto[5][5]={
+                {'*','*','_','*','*'},
+                {'I','*','_','_','_'},
+                {'_','*','_','*','_'},
+                {'_','D','_','*','_'},
+                {'_','*','_','*','_'},
+                };
+
+    cout<<laberinto[1][0]<<endl;
+    cout<<laberinto[3][1]<<endl;
+
+//    cout<<existeCamino(laberinto,
+//                       0,1,
+//                       1,3)<<endl;
+
+    for(int y=0;y<5;y++)
+    {
+        for(int x=0;x<5;x++)
+        {
+            cout<<laberinto[y][x]<<' ';
+        }
+        cout<<endl;
+    }
+    return 0;
+}
